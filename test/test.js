c504ce1ef6c63ef2f1aeb59f88bb91a0e03ea9b2
Trevor Bossert <trevor@vurb.com>
2016-06-14 09:02:49 -0700

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,18 @@
+/* global require, describe, it */
+'use strict';
+
+// MODULES //
+
+var chai = require( 'chai' ),
+	lib = require( './../lib' );
+
+
+// VARIABLES //
+
+var expect = chai.expect,
+	assert = chai.assert;
+
+
+// TESTS //
+
+// TODO //
